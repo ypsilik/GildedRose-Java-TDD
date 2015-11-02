bd02ad5f9a4939fbfd4cceb5ffee1b84b0289df5
sebastienjean <baz.jean@gmail.com>
2015-11-02 09:31:11 +0100

    Initial source code and guidelines
    - set up an Eclipse project 
    - added business-logic startup code in src folder
    - added test startup code in test folder
    - added original GildedRose guidelines as PDF

---
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -0,0 +1,30 @@
+package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
+
+import java.util.List;
+
+public class GildedRose
+{
+
+	/*
+	 * The code below is intended to help writing test fixtures
+	 * 
+	 * items = new ArrayList<Item>(); 
+	 * items.add(new Item("+5 Dexterity Vest", 10, 20)); 
+	 * items.add(new Item("Aged Brie", 2, 0)); 
+	 * items.add(new Item("Elixir of the Mongoose", 5, 7)); 
+	 * items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80)); 
+	 * items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)); 
+	 * items.add(new Item("Conjured Mana Cake", 3, 6));
+	 */
+
+	public static void updateItems(List<Item> items)
+	{
+		for (int indexInItemList = 0; indexInItemList < items.size(); indexInItemList++)
+			updateItem(items.get(indexInItemList));
+	}
+
+	public static void updateItem(Item item)
+	{	
+	}
+
+}
---
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -0,0 +1,15 @@
+package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GildedRoseTest
+{
+	@Test
+	public void aSimpleBeginningTest()
+	{
+		assertEquals(true,false);
+	}
+
+}
